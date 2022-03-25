9426f55960b6d032c25e89e2a6501eb11c49bf3d
ChillarAnand <anand21nanda@gmail.com>
2022-03-25 11:11:52 +0530

    feat: Added sandbox doctype

---
+++ frappe_sandbox/frappe_sandbox/doctype/sandbox/sandbox.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, chillaranand and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sandbox', {
+	// refresh: function(frm) {
+
+	// }
+});
